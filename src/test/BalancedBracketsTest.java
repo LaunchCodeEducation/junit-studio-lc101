7b142579a6ce2acd98e06dd54ec9f4d804dbf423
Carly Langlois <cj.langlois.cl@gmail.com>
2019-10-27 17:03:22 -0500

    init commit

---
+++ src/test/BalancedBracketsTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BalancedBracketsTest {
+
+    //TODO: add tests here
+    @Test
+    public void emptyTest() {
+        assertEquals(10, 10, .001);
+    }
+
+
+}
