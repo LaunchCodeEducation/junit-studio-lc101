e648827e688373d22b8600e63f825d93ab7f8391
zlemihl <terence.a.oneill@gmail.com>
2019-11-25 15:02:52 -0600

    cut long comment in BalancedBracket class and pasted in BalancedBracketsTest

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -5,8 +5,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
-
+    /*
+     * The function BalancedBrackets should return true if and only if
+     * the input string has a set of "balanced" brackets.
+     *
+     * That is, whether it consists entirely of pairs of opening/closing
+     * brackets (in that order), none of which mis-nest. We consider a bracket
+     * to be square-brackets: [ or ].
+     *
+     * The string may contain non-bracket characters as well.
+     *
+     * These strings have balanced brackets:
+     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     *
+     * While these do not:
+     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     */
     //TODO: add tests here
+
     @Test
     public void emptyTest() {
         assertEquals(true, true);
