d60d51ed0aa0b17fb4fbdf2c52bd4949e1ace83a
thomaspetty <thomaskpetty@gmail.com>
2020-05-09 07:27:29 -0500

    studio solution

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -22,14 +22,15 @@ public class BalancedBrackets {
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
-        for (char ch : str.toCharArray()) {
-            if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
-            }
-        }
-        return brackets == 0;
+//        int brackets = 0;
+//        for (char ch : str.toCharArray()) {
+//            if (ch == '[') {
+//                brackets++;
+//            } else if (ch == ']') {
+//                brackets--;
+//            }
+//        }
+//        return brackets == 0;
+        return str.matches("(.*)\\[(.*)\\](.*)");
     }
 }
