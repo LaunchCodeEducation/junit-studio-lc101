08c167be34756715bd77070afe2b0de81e5dfe0e
Alex Eichhorn <aotran@gmail.com>
2021-09-16 19:22:07 -0500

    Single open/close bracket test

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,30 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
-
+/*
+         * These strings have balanced brackets:
+            *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+            *
+            * While these do not:
+            *   "[LaunchCode", "Launch]Code[", "[", "]["
+            */
     //TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void oneSetBracketTest() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+
 
 
 }
