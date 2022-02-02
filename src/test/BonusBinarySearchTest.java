3e04c142eb7a48044938a2ecda651c6cadf706d6
Scott Thoreson <scott.thoreson@live.com>
2022-02-02 16:32:27 -0500

    Started working on BonusBinarySearchTest class tests

--- src/test/BonusBinarySearchTest.java
+++ src/test/BonusBinarySearchTest.java
@@ -1,4 +1,18 @@
 package test;
 
+import org.junit.Before;
+import main.BonusBinarySearch;
+import org.junit.Test;
+
 public class BonusBinarySearchTest {
+    int[] test_array;
+    @Before
+    public void createTestArray(){
+        test_array = new int[15];
+        for(int i = 0;i<test_array.length;i++){
+            test_array[i] = i + 1;
+        }
+    }
+
+
 }
