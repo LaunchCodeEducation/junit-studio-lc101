072cf50391efadd7660d22fd477384549ab627a6
millikr2 <59751763+millikr2@users.noreply.github.com>
2022-02-02 16:24:09 -0500

    testing for bracket pairs

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,35 +1,49 @@
 package main;
 
-
 public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
-     *
+     * <p>
      * That is, whether it consists entirely of pairs of opening/closing
      * brackets (in that order), none of which mis-nest. We consider a bracket
      * to be square-brackets: [ or ].
-     *
+     * <p>
      * The string may contain non-bracket characters as well.
-     *
+     * <p>
      * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
+     * "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     * <p>
      * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     * "[LaunchCode", "Launch]Code[", "[", "]["
      *
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
+        StringBuilder bracketsStringBuilder = new StringBuilder();
         int brackets = 0;
+        boolean matchedSet = false;
+        int count = 0;
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
+                bracketsStringBuilder.append("[");
                 brackets++;
+                count++;
             } else if (ch == ']') {
                 brackets--;
+                bracketsStringBuilder.append("]");
+                count++;
+            }
+            if (count == 2 && bracketsStringBuilder.toString().equals("[]")) {
+                matchedSet = true;
+                count = 0;
+                bracketsStringBuilder.delete(0, bracketsStringBuilder.length());
+            } else if (count == 2) {
+                matchedSet = false;
+                count = 0;
             }
         }
-        return brackets == 0;
+        return matchedSet && brackets == 0;
     }
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,10 +1,13 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+
 
     //TODO: add tests here
     @Test
@@ -12,5 +15,68 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void bracketsWithCharsInsideIsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launchcode]"));
+    }
+
+    @Test
+    public void outputIsFalseWithOneBracketMissing() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launchcode"));
+    }
+
+    @Test
+    public void outputIsFalseWithNoBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launchcode"));
+    }
+
+    @Test
+    public void outputIsFalseWithBackwardsBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Launchcode["));
+    }
+
+    @Test
+    public void outputIsFalseWithMismatchedBracketsWithOneMatchedBracketMiddle() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("}}}]]][asdf2!PPP{{{]]]{"));
+    }
+
+    @Test
+    public void outputIsFalseWithMismatchedBracketsWithOneMatchedBracketFirstEndingBracketOpposite() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[ghj]ghj[fgjh"));
+    }
+
+    @Test
+    public void outputIsFalseWithMismatchedBracketsWithOneMatchedBracketFirstEndingBracketSame() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[ghj]ghj]fgjh"));
+    }
+
+    @Test
+    public void outputIsFalseWithMismatchedBracketsWithOneMatchedBracketEnd() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[hgj[ghj]gfjh"));
+    }
+
+    @Test
+    public void outputIsFalseWithMismatchedBracketsWithOneMatchedBracketEndMismatchedOpposite() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]hgj[ghj]gfjh"));
+    }
+
+    @Test
+    public void outputIsTrueWithMultipleMatchedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("adsf[fgjh]asd[dgh]asdf"));
+    }
+
+    @Test
+    public void outputIsFalseWithSameNumberOfEachBracketAndOneMatch() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]][[[[]"));
+    }
+
+    @Test
+    public void outputIsFalseWithSameNumberOfEachBracketAndOneMatchAtBeginning() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]]][[["));
+    }
 }
