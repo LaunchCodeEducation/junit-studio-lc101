18ec102a3e507c39f547d867bacec51e128d7291
Scott Thoreson <scott.thoreson@live.com>
2022-02-03 09:44:47 -0500

    Added a test to see if n is greater than the last integer in the sorted_array to the BonusBinarySearchTest class

--- src/test/BonusBinarySearchTest.java
+++ src/test/BonusBinarySearchTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BonusBinarySearchTest {
-    int[] test_array;
+    int[] test_array = new int[15];
     @Before
     public void createTestArray(){
         test_array = new int[15];
@@ -17,9 +17,19 @@ public class BonusBinarySearchTest {
     }
 
     @Test
-    public void negativeNumberReturnsFalse(){
-        assertFalse(-5 < 1 && -5 < 16);
+    public void negativeNumberReturnsTrue(){
+        assertTrue(BonusBinarySearch.binarySearch(test_array,-5) == -1);
 //        assertEquals(-1,BonusBinarySearch.binarySearch(test_array,-5),.001);
     }
+    @Test
+    public void greaterThanValueAtLastIndexReturnsTrue(){
+        assertTrue(BonusBinarySearch.binarySearch(test_array,(test_array.length + 1)) == -1);
+    }
+//    @Test
+//    public void nIsInArrayOfIntsReturnsTrue(){
+//        assertTrue(BonusBinarySearch.binarySearch(test_array,8) != -1);
+//    }
+
+
 
 }
